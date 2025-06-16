fe3ea8dcc4a1d8c90e6d668964781dac03f226a5
BestAchadirect <best@achadirect.com>
2025-06-16 17:31:34 +0700

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch Test-16-06-2025
     Your branch is up to date with 'origin/Test-16-06-2025'.
    
     Changes to be committed:
    	modified:   backend/app/__pycache__/__init__.cpython-312.pyc
    	modified:   backend/app/__pycache__/database.cpython-312.pyc
    	modified:   backend/app/__pycache__/models.cpython-312.pyc
    	modified:   backend/app/__pycache__/routes.cpython-312.pyc
    	modified:   backend/main.py
    	modified:   frontend/public/script.js

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,17 @@
 import sys
 import os
 from app.database import engine, Base
+from app.__init__ import create_app
 
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-from app.__init__ import create_app
+
+
+from flask_cors import CORS
 
 app = create_app()
+CORS(app, origins=["http://localhost:5000"])  # Adjust the origin as needed
 
 # Create all tables
 Base.metadata.create_all(bind=engine)
